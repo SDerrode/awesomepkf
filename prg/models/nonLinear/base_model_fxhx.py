d4ed3f4b02802fb9707db1c396925c5a300e96f0
Stéphane Derrode <stephane.derrode@ec-lyon.fr>
2026-03-08 09:54:54 +0100

    continuation sur simpy

--- prg/models/nonLinear/base_model_fxhx.py
+++ prg/models/nonLinear/base_model_fxhx.py
@@ -37,6 +37,12 @@ class BaseModelFxHx(BaseModelNonLinear):
 
         # modèle fourni par la classe fille
         self._sfx, self._shx = self.symbolic_model(self._sx, self._st, self._su)
+        for name, mat in [("sfx", self._sfx), ("shx", self._shx)]:
+            if not isinstance(mat, sp.Matrix) or mat.shape != (1, 1):
+                raise ValueError(
+                    f"symbolic_model() doit retourner des sp.Matrix(1,1), "
+                    f"got {type(mat)} shape={getattr(mat, 'shape', '?')} for {name}"
+                )
         # print(self._sfx)
         # print(self._shx)
         # input("ATTENTE")
@@ -52,19 +58,11 @@ class BaseModelFxHx(BaseModelNonLinear):
         self._fx_num = sp.lambdify((self._sx, self._st), self._sfx[0, 0], "numpy")
         self._hx_num = sp.lambdify((self._sx, self._su), self._shx[0, 0], "numpy")
 
-        try:
-            self._A_num = sp.lambdify((self._sx,), self._sA[0, 0], "numpy")
-            self._H_num = sp.lambdify((self._sx,), self._sH[0, 0], "numpy")
-            # print(self._A_num)
-            # print(self._H_num)
-            # input("ATTENTE 4")
-
-        except Exception as e:
-            import traceback
-
-            print("[ERREUR] dans symbolic_model:")
-            print(traceback.format_exc())
-            return None, None
+        self._A_num = sp.lambdify((self._sx,), self._sA[0, 0], "numpy")
+        self._H_num = sp.lambdify((self._sx,), self._sH[0, 0], "numpy")
+        # print(self._A_num)
+        # print(self._H_num)
+        # input("ATTENTE 4")
 
     # ------------------------------------------------------------------
 
@@ -75,29 +73,37 @@ class BaseModelFxHx(BaseModelNonLinear):
         return self._hx_num(x, u)
 
     def _jacobiens_g(self, x, y, t, u, dt):
+        try:
+            with np.errstate(all="raise"):
 
-        dfdx = self._A_num(x)  # df/dx évalué en x
-        dhdx = self._H_num(x)  # dh/dx évalué en x
-        # dh/dx de g_y = hx(fx(x)) par rapport à x = dhdx * dfdx (règle chaîne)
-        chain = dhdx * dfdx
+                if x.ndim == 2:
+                    x0 = x[0, 0]
+                    t0 = t[0, 0]
+                    fx_val = float(self._fx_num(x0, t0))  # A = fx(x)
+                    dfdx = float(self._A_num(x0))  # df/dx en x
+                    dhdx = float(self._H_num(fx_val))  # dh/dx en fx(x) ← correction clé
+                    An = np.array([[dfdx, 0.0], [dhdx * dfdx, 0.0]])
+                    Bn = np.array(
+                        [[1.0, 0.0], [dhdx, 1.0]]
+                    )  # dhdx ici, pas sA hardcodé
+                else:
+                    N = x.shape[0]
+                    x0 = x[:, 0, 0]
+                    t0 = t[:, 0, 0]
+                    fx_val = self._fx_num(x0, t0)
+                    dfdx = self._A_num(x0)
+                    dhdx = self._H_num(fx_val)  # ← évalué en fx(x)
+                    An = np.zeros((N, 2, 2))
+                    An[:, 0, 0] = dfdx
+                    An[:, 1, 0] = dhdx * dfdx
+                    Bn = np.tile(np.eye(2), (N, 1, 1))
+                    Bn[:, 1, 0] = dhdx
+                return An, Bn
 
-        if x.ndim == 2:
-            # cas 2D : scalaires
-            a = float(np.squeeze(dfdx))
-            c = float(np.squeeze(chain))
-            An = np.array([[a, 0.0], [c, 0.0]])
-            Bn = np.array([[1.0, 0.0], [0.0, 1.0]])
-        else:
-            # cas batch : x shape (N, 1, 1)
-            N = x.shape[0]
-            a = np.squeeze(dfdx)  # shape (N,)
-            c = np.squeeze(chain)  # shape (N,)
-            An = np.zeros((N, 2, 2))
-            An[:, 0, 0] = a
-            An[:, 1, 0] = c
-            Bn = np.tile(np.eye(2), (N, 1, 1))
-
-        return An, Bn
+        except FloatingPointError as e:
+            raise NumericalError(
+                f"[{self.__class__.__name__}] _jacobiens_g: erreur numérique: {e}"
+            ) from e
 
     def latex_model(self):
 
