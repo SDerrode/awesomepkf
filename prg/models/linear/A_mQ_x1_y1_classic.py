72240d9c97d28929c767bd78d5e0e804ff3b7b19
Stéphane Derrode <stephane.derrode@ec-lyon.fr>
2026-03-10 04:39:28 +0100

    ajout modele lineai non pairwise : A_mQ_x1_y1_classic

--- prg/models/linear/A_mQ_x1_y1_classic.py
+++ prg/models/linear/A_mQ_x1_y1_classic.py
@@ -4,16 +4,16 @@
 import numpy as np
 from prg.models.linear.base_model_linear import LinearAmQ
 
-__all__ = ["Model_A_mQ_x1_y1"]
+__all__ = ["A_mQ_x1_y1_classic"]
 
 
-class Model_A_mQ_x1_y1(LinearAmQ):
+class Model_A_mQ_x1_y1_classic(LinearAmQ):
 
-    MODEL_NAME = "A_mQ_x1_y1"
+    MODEL_NAME = "A_mQ_x1_y1_classic"
 
     def __init__(self):
         dim_x, dim_y = 1, 1
-        A = np.array([[0.95, -0.3], [0.2, 0.85]])
+        A = np.array([[0.95, 0.0], [0.2, 0.0]])
         mQ, mz0, Pz0 = LinearAmQ._init_random_params(dim_x, dim_y, val_max=0.15)
         super().__init__(
             dim_x=dim_x,
